260221e08cc5fb7e7059490963198ac93d92f298
Christopher Siems and Kassidy Douglas <csiems@gmail.com>
2016-03-14 09:59:00 -0700

    Add home page and form

--- app/src/main/java/com/epicodus/myspace/UserPageActivity.java
+++ app/src/main/java/com/epicodus/myspace/UserPageActivity.java
@@ -3,13 +3,11 @@ package com.epicodus.myspace;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    private 
+public class UserPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_page);
     }
 }
