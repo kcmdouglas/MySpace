ff8a98744e203d068ccf2ff31b8d38f5eb0967e1
Christopher Siems and Kassidy Douglas <kcmdouglas@gmail.com>
2016-03-14 14:09:51 -0700

    add grid layout to user page

--- app/src/main/java/com/epicodus/myspace/MainActivity.java
+++ app/src/main/java/com/epicodus/myspace/MainActivity.java
@@ -1,15 +1,27 @@
 package com.epicodus.myspace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private 
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+            mLoginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, UserPageActivity.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
