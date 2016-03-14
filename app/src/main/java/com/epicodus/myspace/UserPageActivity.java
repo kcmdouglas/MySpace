39fc6e007e646329bb6d283263070e853bf75e50
Christopher Siems and Kassidy Douglas <csiems@gmail.com>
2016-03-14 16:34:23 -0700

    add friends page

--- app/src/main/java/com/epicodus/myspace/UserPageActivity.java
+++ app/src/main/java/com/epicodus/myspace/UserPageActivity.java
@@ -1,13 +1,27 @@
 package com.epicodus.myspace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserPageActivity extends AppCompatActivity {
+
+    private Button mMorePicsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
+
+        mMorePicsButton = (Button) findViewById(R.id.morePicsButton);
+            mMorePicsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(UserPageActivity.this, ImageActivity.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
